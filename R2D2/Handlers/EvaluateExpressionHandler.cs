5e3c614ba09b3df3156ebfa47204c0a55e562e99
charconstpointer <attwistingnether@gmail.com>
2019-11-26 09:30:54 +0100

    cache

--- R2D2/Handlers/EvaluateExpressionHandler.cs
+++ R2D2/Handlers/EvaluateExpressionHandler.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
-using Microsoft.Extensions.Caching.Memory;
 using R2D2.Commands;
 using R2D2.Interfaces;
 
@@ -11,23 +9,15 @@ namespace R2D2.Handlers
     public class EvaluateExpressionHandler : IRequestHandler<EvaluateExpression, double>
     {
         private readonly IExpressionCalculator _calculator;
-        private readonly IMemoryCache _cache;
 
-        public EvaluateExpressionHandler(IExpressionCalculator calculator, IMemoryCache cache)
+        public EvaluateExpressionHandler(IExpressionCalculator calculator)
         {
             _calculator = calculator;
-            _cache = cache;
         }
 
         public Task<double> Handle(EvaluateExpression request, CancellationToken cancellationToken)
         {
-            if (_cache.TryGetValue(request.Expression, out var cachedValue))
-            {
-                return Task.FromResult((double) cachedValue);
-            }
-
             var value = _calculator.Evaluate(request.Expression);
-            _cache.Set(request.Expression, value, TimeSpan.FromMinutes(5));
             return Task.FromResult(value);
         }
     }
